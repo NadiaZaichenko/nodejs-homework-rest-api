4742aa4236035bdaf174108d1a5868c649a3275a
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-07-12 10:00:06 +0300

    fix validate email && phone

--- shemas/shemaContact.js
+++ shemas/shemaContact.js
@@ -2,9 +2,21 @@
 const Joi = require("joi");
 
 const addShema = Joi.object({
-     name: Joi.string().required(),
-     email: Joi.string().required(),
-     phone: Joi.string().required(),
+     name: Joi.string()
+     .min(4)
+     .max(30)
+     .required()
+     .messages({ "any.required": "missing required fields" }),
+     email: Joi.string()
+     .email()
+     .required()
+     .messages({"any.required" : "missing required fields"}),
+     phone: Joi.string() .pattern(
+        /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+        { name: "numbers" }
+      )
+      .required()
+      .messages({ "any.required": "missing required fields" }),
 })
 
 module.exports = {
